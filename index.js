a09c10a35dd50131bf4f089318b7bd29444d69fb
Adrian1690 <j.adrian.1690@gmail.com>
2017-01-06 19:01:31 -0500

    sending whisper messages

--- index.js
+++ index.js
@@ -9,16 +9,74 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+// sending to sender-client only
+//socket.emit('message', "this is a test");
+
+// sending to all clients, include sender
+//io.emit('message', "this is a test");
+
+// sending to all clients except sender
+//socket.broadcast.emit('message', "this is a test");
+
+// sending to all clients in 'game' room(channel) except sender
+//socket.broadcast.to('game').emit('message', 'nice game');
+
+// sending to all clients in 'game' room(channel), include sender
+//io.in('game').emit('message', 'cool game');
+
+// sending to sender client, only if they are in 'game' room(channel)
+//socket.to('game').emit('message', 'enjoy the game');
+
+// sending to all clients in namespace 'myNamespace', include sender
+//io.of('myNamespace').emit('message', 'gg');
+
+// sending to individual socketid
+//socket.broadcast.to(socketid).emit('message', 'for your eyes only');
+
 users = [];
 connections = [];
+
+socketUsers = [];
+
 io.on('connection', function(socket){
 	
 	connections.push(socket);
 	console.log('user conected : ' + connections.length);
 
+
+	//send menssage
 	socket.on('chat message', function(msg){
+		
+		msg = msg.trim();
 		console.log('message sended : ' + msg);
-		io.emit('chat message', { user: socket.username , message: msg} );
+
+		if( msg.substring(0,3) === '/w ' ){ // if is whisper
+			
+			msg = msg.substring(3);
+			var ind = msg.indexOf(' ');
+			
+			if( ind !== -1 ){
+				var username = msg.substring(0, ind);// nickname;
+				username = username.trim();
+				msg = msg.substring(ind + 1);
+
+				if( username in socketUsers ){
+					console.log('valid name');
+					console.log(socket.username);
+					socket.broadcast
+						.to(socketUsers[username].id)
+						.emit('whisper', { user: socket.username , message: msg});
+				}else{
+					console.log('Error: send to valid username');
+				}
+
+			}else{
+				console.log('Send message to whisper.');
+			}
+		}else{
+			io.emit('chat message', { user: socket.username , message: msg} );
+			//socket.broadcast.emit('chat message', { user: socket.username , message: msg} ); // send all except me
+		}
 	});
 
 	socket.on('disconnect', function(){
@@ -28,15 +86,21 @@ io.on('connection', function(socket){
 
 		console.log('user disconnect');
 		connections.splice(connections.indexOf(socket), 1);
+		//delete users[socket.username];
 		console.log('user conected : ' + connections.length);
+		//updateUsernames();
 	});
 
 	// new user
 	socket.on('new user', function(data, callback){
 		callback(true);
+		data =  data.trim();
 		socket.username = data;
 		users.push(socket.username);
+
+		socketUsers[socket.username] = socket;
 		updateUsernames();
+
 	});
 
 	function updateUsernames(){
